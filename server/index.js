fc59b82fc11804537c38e3b4d115dadb53dec722
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-19 23:35:06 +0530

    index file changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : '*',
     credentials : true
 }))
 app.use(express.json())
