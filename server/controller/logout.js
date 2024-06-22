f0ca48342c6e9c83578ba806109dc9b084d3dbeb
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-22 16:24:20 +0530

    server changes

--- server/controller/logout.js
+++ server/controller/logout.js
@@ -1,15 +1,14 @@
 async function logout(request,response){
     try {
-        // const cookieOptions = {
-        //     http : true,
-        //     secure : true
-        // }
+        const cookieOptions = {
+            http : true,
+            secure : true
+        }
 
-    //     return response.cookie('token','',cookieOptions).status(200).json({
-    //         message : "session out",
-    //         success : true
-    // })
-      return response.status(200)
+        return response.cookie('token','',cookieOptions).status(200).json({
+            message : "session out",
+            success : true
+    })
     } catch (error) {
         return response.status(500).json({
             message : error.message || error,
