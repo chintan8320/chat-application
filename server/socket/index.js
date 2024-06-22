26792614b5f07b084229d939bb1409a7b7798b3e
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-22 13:45:52 +0530

    Bug fixes

--- server/socket/index.js
+++ server/socket/index.js
@@ -33,7 +33,7 @@ io.on('connection',async(socket)=>{
     const user = await getUserDetailsFromToken(token)
 
     //create a room
-    socket.join(user?._id.toString())
+    socket.join(user?._id?.toString())
     onlineUser.add(user?._id?.toString())
 
     io.emit('onlineUser',Array.from(onlineUser))
