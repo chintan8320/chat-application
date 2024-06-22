087d33ae5857478897f73f4b06a436943802298c
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-22 13:58:48 +0530

    password changes

--- server/controller/checkPassword.js
+++ server/controller/checkPassword.js
@@ -23,17 +23,12 @@ async function checkPassword(request,response){
         }
         const token = await jwt.sign(tokenData,process.env.JWT_SECREAT_KEY,{ expiresIn : '1d'})
 
-        const cookieOptions = {
-            http : true,
-            secure : true
-        }
+        
 
-        return response.cookie('token',token,cookieOptions).status(200).json({
-            message : "Login successfully",
-            token : token,
-            success :true
+        return response.status(200).json({
+            message : "correct password",
+            token: token
         })
-
     } catch (error) {
         return response.status(500).json({
             message : error.message || error,
