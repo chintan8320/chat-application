c464855676757faa1cb4b54466819f90def9b6a1
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-22 13:36:47 +0530

    changes

--- server/socket/index.js
+++ server/socket/index.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
-        origin : process.env.FRONTEND_URL,
+        origin : '*',
         credentials : true
     }
 })
